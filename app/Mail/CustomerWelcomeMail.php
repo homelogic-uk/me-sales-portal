841265c25400e148ab2c2d286e0cfee1e7b7409f
Ashley <ashley@myenergy.co.uk>
2026-03-05 12:06:57 +0000

    Fix

--- app/Mail/CustomerWelcomeMail.php
+++ app/Mail/CustomerWelcomeMail.php
@@ -47,7 +47,7 @@ class CustomerWelcomeMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'mail.external-contract',
+            view: 'mail.welcome',
         );
     }
 
